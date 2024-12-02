553908394ddf1c4aed59dbb347d72359cae0c839
gilberttetteh <144915635+gilberttetteh@users.noreply.github.com>
2024-12-02 20:48:09 +0000

    Add files via upload

--- Uni Management/FacCOurses.h
+++ Uni Management/FacCOurses.h
@@ -51,11 +51,13 @@ namespace UniManagement {
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn1;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn2;
 	private: System::Windows::Forms::DataGridViewTextBoxColumn^ dataGridViewTextBoxColumn3;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
 	private: System::Windows::Forms::Button^ btnVerifyEnrollment;
 
 #pragma region Windows Form Designer generated code
 		   void InitializeComponent(void)
 		   {
+			   System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(FacCOurses::typeid));
 			   this->sidebarPanel = (gcnew System::Windows::Forms::Panel());
 			   this->btnDashboard = (gcnew System::Windows::Forms::Button());
 			   this->cbCourseSelection = (gcnew System::Windows::Forms::ComboBox());
@@ -69,33 +71,37 @@ namespace UniManagement {
 			   this->dataGridViewTextBoxColumn1 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			   this->dataGridViewTextBoxColumn2 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
 			   this->dataGridViewTextBoxColumn3 = (gcnew System::Windows::Forms::DataGridViewTextBoxColumn());
+			   this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
 			   this->sidebarPanel->SuspendLayout();
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgvRoster))->BeginInit();
+			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			   this->SuspendLayout();
 			   // 
 			   // sidebarPanel
 			   // 
-			   this->sidebarPanel->BackColor = System::Drawing::Color::Firebrick;
+			   this->sidebarPanel->BackColor = System::Drawing::Color::Maroon;
+			   this->sidebarPanel->Controls->Add(this->pictureBox1);
 			   this->sidebarPanel->Controls->Add(this->btnDashboard);
 			   this->sidebarPanel->Dock = System::Windows::Forms::DockStyle::Left;
 			   this->sidebarPanel->Location = System::Drawing::Point(0, 0);
 			   this->sidebarPanel->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			   this->sidebarPanel->Name = L"sidebarPanel";
-			   this->sidebarPanel->Size = System::Drawing::Size(300, 950);
+			   this->sidebarPanel->Size = System::Drawing::Size(247, 950);
 			   this->sidebarPanel->TabIndex = 0;
 			   // 
 			   // btnDashboard
 			   // 
+			   this->btnDashboard->BackColor = System::Drawing::Color::WhiteSmoke;
 			   this->btnDashboard->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			   this->btnDashboard->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
-			   this->btnDashboard->ForeColor = System::Drawing::Color::White;
-			   this->btnDashboard->Location = System::Drawing::Point(45, 62);
+			   this->btnDashboard->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 7.875F, System::Drawing::FontStyle::Bold));
+			   this->btnDashboard->ForeColor = System::Drawing::Color::Black;
+			   this->btnDashboard->Location = System::Drawing::Point(13, 818);
 			   this->btnDashboard->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
 			   this->btnDashboard->Name = L"btnDashboard";
 			   this->btnDashboard->Size = System::Drawing::Size(210, 78);
 			   this->btnDashboard->TabIndex = 0;
-			   this->btnDashboard->Text = L"Dashboard";
-			   this->btnDashboard->UseVisualStyleBackColor = true;
+			   this->btnDashboard->Text = L"BACK";
+			   this->btnDashboard->UseVisualStyleBackColor = false;
 			   this->btnDashboard->Click += gcnew System::EventHandler(this, &FacCOurses::btnDashboard_Click);
 			   // 
 			   // cbCourseSelection
@@ -103,18 +109,18 @@ namespace UniManagement {
 			   this->cbCourseSelection->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
 			   this->cbCourseSelection->FormattingEnabled = true;
 			   this->cbCourseSelection->Items->AddRange(gcnew cli::array< System::Object^  >(3) { L"Math 101", L"Physics 102", L"Chemistry 103" });
-			   this->cbCourseSelection->Location = System::Drawing::Point(360, 50);
+			   this->cbCourseSelection->Location = System::Drawing::Point(250, 3);
 			   this->cbCourseSelection->Name = L"cbCourseSelection";
 			   this->cbCourseSelection->Size = System::Drawing::Size(300, 45);
 			   this->cbCourseSelection->TabIndex = 1;
 			   // 
 			   // btnUploadMaterial
 			   // 
-			   this->btnUploadMaterial->BackColor = System::Drawing::Color::Crimson;
+			   this->btnUploadMaterial->BackColor = System::Drawing::Color::Maroon;
 			   this->btnUploadMaterial->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			   this->btnUploadMaterial->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
 			   this->btnUploadMaterial->ForeColor = System::Drawing::Color::White;
-			   this->btnUploadMaterial->Location = System::Drawing::Point(1102, 111);
+			   this->btnUploadMaterial->Location = System::Drawing::Point(1119, 54);
 			   this->btnUploadMaterial->Name = L"btnUploadMaterial";
 			   this->btnUploadMaterial->Size = System::Drawing::Size(249, 57);
 			   this->btnUploadMaterial->TabIndex = 2;
@@ -124,11 +130,11 @@ namespace UniManagement {
 			   // 
 			   // btnViewRoster
 			   // 
-			   this->btnViewRoster->BackColor = System::Drawing::Color::Crimson;
+			   this->btnViewRoster->BackColor = System::Drawing::Color::Maroon;
 			   this->btnViewRoster->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			   this->btnViewRoster->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
 			   this->btnViewRoster->ForeColor = System::Drawing::Color::White;
-			   this->btnViewRoster->Location = System::Drawing::Point(360, 111);
+			   this->btnViewRoster->Location = System::Drawing::Point(250, 54);
 			   this->btnViewRoster->Name = L"btnViewRoster";
 			   this->btnViewRoster->Size = System::Drawing::Size(245, 57);
 			   this->btnViewRoster->TabIndex = 3;
@@ -143,15 +149,15 @@ namespace UniManagement {
 				   this->dataGridViewTextBoxColumn1,
 					   this->dataGridViewTextBoxColumn2, this->dataGridViewTextBoxColumn3
 			   });
-			   this->dgvRoster->Location = System::Drawing::Point(307, 164);
+			   this->dgvRoster->Location = System::Drawing::Point(250, 104);
 			   this->dgvRoster->Name = L"dgvRoster";
 			   this->dgvRoster->RowHeadersWidth = 82;
-			   this->dgvRoster->Size = System::Drawing::Size(1087, 721);
+			   this->dgvRoster->Size = System::Drawing::Size(1154, 792);
 			   this->dgvRoster->TabIndex = 4;
 			   // 
 			   // btnSaveMaterial
 			   // 
-			   this->btnSaveMaterial->BackColor = System::Drawing::Color::Crimson;
+			   this->btnSaveMaterial->BackColor = System::Drawing::Color::Maroon;
 			   this->btnSaveMaterial->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			   this->btnSaveMaterial->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
 			   this->btnSaveMaterial->ForeColor = System::Drawing::Color::White;
@@ -166,7 +172,7 @@ namespace UniManagement {
 			   // txtMaterialName
 			   // 
 			   this->txtMaterialName->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
-			   this->txtMaterialName->Location = System::Drawing::Point(360, 430);
+			   this->txtMaterialName->Location = System::Drawing::Point(305, 440);
 			   this->txtMaterialName->Name = L"txtMaterialName";
 			   this->txtMaterialName->Size = System::Drawing::Size(300, 43);
 			   this->txtMaterialName->TabIndex = 7;
@@ -175,7 +181,7 @@ namespace UniManagement {
 			   // 
 			   this->lblMaterialName->AutoSize = true;
 			   this->lblMaterialName->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10));
-			   this->lblMaterialName->Location = System::Drawing::Point(360, 400);
+			   this->lblMaterialName->Location = System::Drawing::Point(300, 400);
 			   this->lblMaterialName->Name = L"lblMaterialName";
 			   this->lblMaterialName->Size = System::Drawing::Size(199, 37);
 			   this->lblMaterialName->TabIndex = 6;
@@ -183,7 +189,7 @@ namespace UniManagement {
 			   // 
 			   // btnVerifyEnrollment
 			   // 
-			   this->btnVerifyEnrollment->BackColor = System::Drawing::Color::Crimson;
+			   this->btnVerifyEnrollment->BackColor = System::Drawing::Color::Maroon;
 			   this->btnVerifyEnrollment->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
 			   this->btnVerifyEnrollment->Font = (gcnew System::Drawing::Font(L"Segoe UI", 10, System::Drawing::FontStyle::Bold));
 			   this->btnVerifyEnrollment->ForeColor = System::Drawing::Color::White;
@@ -216,6 +222,16 @@ namespace UniManagement {
 			   this->dataGridViewTextBoxColumn3->Name = L"dataGridViewTextBoxColumn3";
 			   this->dataGridViewTextBoxColumn3->Width = 200;
 			   // 
+			   // pictureBox1
+			   // 
+			   this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			   this->pictureBox1->Location = System::Drawing::Point(3, 12);
+			   this->pictureBox1->Name = L"pictureBox1";
+			   this->pictureBox1->Size = System::Drawing::Size(231, 214);
+			   this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::StretchImage;
+			   this->pictureBox1->TabIndex = 1;
+			   this->pictureBox1->TabStop = false;
+			   // 
 			   // FacCOurses
 			   // 
 			   this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
@@ -235,6 +251,7 @@ namespace UniManagement {
 			   this->Text = L"Manage Courses";
 			   this->sidebarPanel->ResumeLayout(false);
 			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dgvRoster))->EndInit();
+			   (cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			   this->ResumeLayout(false);
 			   this->PerformLayout();
 
