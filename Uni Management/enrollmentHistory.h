7a0d287a90082371ef2c650960b60677c3babc95
sedemabla <sedemagudetse3@gmail.com>
2024-12-02 22:25:35 +0000

    Add files via upload

--- Uni Management/enrollmentHistory.h
+++ Uni Management/enrollmentHistory.h
@@ -34,6 +34,16 @@ namespace UniManagement {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ panel7;
+	protected:
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::PictureBox^ pictureBox2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Panel^ panel1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::PictureBox^ pictureBox3;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
 
 	private:
 		/// <summary>
@@ -48,12 +58,140 @@ namespace UniManagement {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"enrollmentHistory";
-			this->Padding = System::Windows::Forms::Padding(0);
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(enrollmentHistory::typeid));
+			this->panel7 = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->pictureBox2 = (gcnew System::Windows::Forms::PictureBox());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->pictureBox3 = (gcnew System::Windows::Forms::PictureBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->panel7->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->panel1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// panel7
+			// 
+			this->panel7->BackColor = System::Drawing::Color::Maroon;
+			this->panel7->Controls->Add(this->pictureBox3);
+			this->panel7->Controls->Add(this->button1);
+			this->panel7->Controls->Add(this->pictureBox2);
+			this->panel7->Controls->Add(this->pictureBox1);
+			this->panel7->Location = System::Drawing::Point(-22, -289);
+			this->panel7->Name = L"panel7";
+			this->panel7->Size = System::Drawing::Size(265, 1686);
+			this->panel7->TabIndex = 14;
+			this->panel7->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &enrollmentHistory::panel7_Paint);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(34, 1296);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(209, 75);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"BACK";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// pictureBox2
+			// 
+			this->pictureBox2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox2.Image")));
+			this->pictureBox2->Location = System::Drawing::Point(12, 7);
+			this->pictureBox2->Name = L"pictureBox2";
+			this->pictureBox2->Size = System::Drawing::Size(209, 226);
+			this->pictureBox2->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox2->TabIndex = 1;
+			this->pictureBox2->TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(112, 138);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(8, 8);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// panel1
+			// 
+			this->panel1->BackColor = System::Drawing::Color::Transparent;
+			this->panel1->Controls->Add(this->label1);
+			this->panel1->Location = System::Drawing::Point(243, -4);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(1148, 111);
+			this->panel1->TabIndex = 15;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(274, 34);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(601, 55);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"ENROLLMENT HISTORY";
+			// 
+			// pictureBox3
+			// 
+			this->pictureBox3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox3.Image")));
+			this->pictureBox3->Location = System::Drawing::Point(26, 301);
+			this->pictureBox3->Name = L"pictureBox3";
+			this->pictureBox3->Size = System::Drawing::Size(233, 226);
+			this->pictureBox3->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox3->TabIndex = 3;
+			this->pictureBox3->TabStop = false;
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 14.875F, System::Drawing::FontStyle::Bold));
+			this->label2->Location = System::Drawing::Point(256, 193);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(465, 45);
+			this->label2->TabIndex = 16;
+			this->label2->Text = L"PAST ENROLLMENTS :";
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::ActiveBorder;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->GridColor = System::Drawing::SystemColors::ControlLight;
+			this->dataGridView1->Location = System::Drawing::Point(264, 249);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 82;
+			this->dataGridView1->RowTemplate->Height = 33;
+			this->dataGridView1->Size = System::Drawing::Size(1123, 830);
+			this->dataGridView1->TabIndex = 17;
+			// 
+			// enrollmentHistory
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1399, 1108);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->panel1);
+			this->Controls->Add(this->panel7);
+			this->Name = L"enrollmentHistory";
+			this->Text = L"enrollmentHistory";
+			this->panel7->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox2))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox3))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void panel7_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
+	}
+};
 }
